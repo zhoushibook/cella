97f29bedf9765f807b64a4f40ec7c862e76de57c
PinelliaCSU <3218346707@qq.com>
2026-09-15 17:53:17 +0800

    fix:testdata

--- testdata/verify_ok.sql
+++
@@ -1,8 +0,0 @@
-get id, name, city in users ordered id desc among 2;
-get id in orders ordered id desc among 2;
-get id, title in products ordered id desc among 2;
-get tag, val in events ordered val desc among 2;
-get id, city in users limit city is null among 2;
-get id, name in users limit id = 25000;
-get id, name, age in users limit age = 30 among 3;
-get id, user_id in orders limit user_id = 100 among 3;
